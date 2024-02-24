fdd290a0830498f591f22a401520fb7105ab72ee
Philip Levy <pglevy@users.noreply.github.com>
2024-02-24 17:41:14 -0500

    push static site

---
+++ _site/assets/js/main.js
@@ -0,0 +1,7 @@
+// Add JavaScript here
+
+// const audio = document.querySelector("audio");
+
+// audio.ontimeupdate = (event) => {
+//   console.log("The currentTime attribute has been updated: " + audio.currentTime);
+// };  
