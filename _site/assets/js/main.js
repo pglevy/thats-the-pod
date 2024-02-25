bc89c9c256bd0b825a61715c79161855cea19394
Philip Levy <pglevy@users.noreply.github.com>
2024-02-25 15:18:41 -0500

    remove all posts and rebuild site

--- _site/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Add JavaScript here
-
-// const audio = document.querySelector("audio");
-
-// audio.ontimeupdate = (event) => {
-//   console.log("The currentTime attribute has been updated: " + audio.currentTime);
-// };  
